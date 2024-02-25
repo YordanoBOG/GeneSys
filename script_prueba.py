aa2fa5a6610fc9a960399fce97332587be13d4fb
YordanoBOG <68510470+YordanoBOG@users.noreply.github.com>
2024-02-25 18:17:50 +0100

    Add files via upload

--- script_prueba.py
+++ script_prueba.py
@@ -1,3 +1,15 @@
-import pandas
+import subprocess
 
-print ("Hello")
+comando = "ls -l"
+
+resultado = subprocess.run(comando, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+#'''
+# Muestra la salida del comando
+print("Salida del comando:")
+print(resultado.stdout)
+
+# Muestra los errores, si los hay
+print("Errores (si los hay):")
+print(resultado.stderr)
+#'''
